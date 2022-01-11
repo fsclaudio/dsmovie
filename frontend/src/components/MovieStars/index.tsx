a61905a70704442d1f39a78cb8f3070334152e2b
fsclaudio <fsclaudio@gmail.com>
2022-01-11 18:44:31 -0300

    MoviCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,18 @@
+import {ReactComponent as StarFull} from 'assets/img/StarFull.svg';
+import {ReactComponent as StarHalf} from 'assets/img/StarHalf.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/Star.svg';
+import './styles.css';
+function MovieStars() {
+
+    return(
+        <div className="dsmovie-stars-container">
+  <StarFull />
+  <StarFull />
+  <StarFull />
+  <StarHalf />
+  <StarEmpty />
+</div>
+    )
+}
+
+export default MovieStars;
